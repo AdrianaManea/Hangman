d44203242a2a1bd341dabdbca34c4f634d1324e7
Adina <adrianna.manea@gmail.com>
2021-02-22 15:31:08 +0100

    keydown letter press event handler

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
+// sets the wordEl innerHTML 
+// to whatever the selectedWord is, which is generated randomly from our 'words' array
+// split() it into an array
+// map() through it
+// see's if there is a letter in the correctLetters array, if there is it's going to display it, if not it display's just an empty string
+// we run join() to turn it back into a string
+
+// then we check to see if wordEl.innerText matches the selecteWord
+// if it does it means we've guessed the word and the popup displays
+
+
 function displayWord() {
   // if the current letter we're looping through included in corectLetters
   // ? if so, show letter
@@ -41,5 +52,48 @@ function displayWord() {
     popup.style.display = 'flex';
   }
 }
+
+// Update the wrong letters
+function updateWrondLettersEl() {
+  console.log('Update wrong');
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+
+  }, 2000);
+}
+
+// Keydown letter press event handler
+// fire off when we hit a letter
+// letter from a to z = 65 to 90
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    // console.log(123);
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrondLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
